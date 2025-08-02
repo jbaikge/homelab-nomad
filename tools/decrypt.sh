dfb9a76528d2cdb69ecac95ebb7f7a0c0eac4a98
jake <523216+jbaikge@users.noreply.github.com>
2025-08-02 00:22:19 -0400

    refactor: remove tools dir

--- tools/decrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-DIR=$(dirname "$0")
-
-for FILE in $DIR/*.encrypted.yml; do
-    sops decrypt --output "${FILE/.encrypted.yml/.decrypted.yml}" "$FILE"
-done
--- tools/encrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-DIR=$(realpath $(dirname "$0")/..)
-
-for FILE in $DIR/**/*.decrypted.yml; do
-    sops encrypt --output "${FILE/.decrypted.yml/.encrypted.yml}" "$FILE"
-done
