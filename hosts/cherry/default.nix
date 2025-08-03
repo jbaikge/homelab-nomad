85a5e94314b1e07d43a56b29e6abd3655b1915dc
jake <523216+jbaikge@users.noreply.github.com>
2025-08-03 08:49:22 -0400

    feat: defining sibling nodes

--- hosts/cherry/default.nix
+++ hosts/cherry/default.nix
@@ -10,4 +10,9 @@
   networking.hostName = "cherry";
   users.users.jake.initialPassword = "cherry";
   services.consul.extraConfig.node_name = "cherry";
+  services.nomad.settings.server.bootstrap_expect = 3;
+  services.nomad.settings.server.server_join.retry_join = [
+    "hickory.hardwood.cloud"
+    "mapple.hardwood.cloud"
+  ];
 }
