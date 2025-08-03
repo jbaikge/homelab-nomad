90e6a17e91e246e6a81a1e0fc4bd194b67738718
jake <523216+jbaikge@users.noreply.github.com>
2025-08-03 00:16:48 -0400

    refactor: simplify with new default.nix files

--- flake.nix
+++ flake.nix
@@ -1,11 +1,19 @@
 {
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  description = "Nomad homelab cluster";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.05";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs =
     { nixpkgs, disko, ... }:
+    let
+      clusterConfig = {
+        serverNames = [
+          "cherry.hardwood.cloud"
+        ];
+      };
+    in
     {
       nixosConfigurations = {
         iso = nixpkgs.lib.nixosSystem {
@@ -13,16 +21,16 @@
           modules = [
             "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
             "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-            ./hosts/iso/configuration.nix
+            ./hosts/iso
           ];
         };
         cherry = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             disko.nixosModules.disko
-            ./hosts/cherry/configuration.nix
-            ./hosts/cherry/hardware-configuration.nix
+            ./hosts/cherry
           ];
+          specialArgs = { inherit clusterConfig; };
         };
       };
     };
