9057bc2d6d5fd8170912586127390f5cd6005250
jake <523216+jbaikge@users.noreply.github.com>
2025-08-03 00:46:27 -0400

    feat: add consul configuration

--- hosts/cherry/default.nix
+++ hosts/cherry/default.nix
@@ -3,10 +3,11 @@
     ../common
     ./disko.nix
     ./hardware-configuration.nix
-    # ../../services/consul
+    ../../services/consul
     ../../services/nomad
   ];
 
   networking.hostName = "cherry";
   users.users.jake.initialPassword = "cherry";
+  services.consul.extraConfig.node_name = "cherry";
 }
---
+++ services/consul/default.nix
@@ -0,0 +1,57 @@
+{ pkgs, ... }:
+{
+  services.consul = {
+    enable = true;
+    package = pkgs.consul;
+
+    extraConfig = {
+      server = true;
+      rejoin_after_leave = true;
+      ui = true;
+      ui_config.enabled = true;
+
+      client_addr = "0.0.0.0";
+      # TODO figure out advertise addr
+      # advertise_addr = "";
+      # TODO bump this to number of nodes
+      bootstrap_expect = 1;
+
+      datacenter = "dc1";
+      node_name = "unknown";
+
+      encrypt = "pJgXpTzlDm3zC9GFoM5jtP9/TYsY1EikpGtPGJP6sOU=";
+
+      connect = {
+        enabled = true;
+      };
+
+      ports = {
+        grpc = 8502;
+        dns = 8600;
+      };
+
+      recursors = [
+        "1.1.1.1"
+        "1.0.0.1"
+        "8.8.8.8"
+      ];
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    8500
+    8501
+    8502
+    8503
+    8600
+    8300
+    8301
+    8302
+  ];
+  networking.firewall.allowedUDPPorts = [
+    8600
+    8300
+    8301
+    8302
+  ];
+}
