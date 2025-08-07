fbd52cdf361308866679842837fee13e604e8c71
jake <523216+jbaikge@users.noreply.github.com>
2025-08-07 08:27:18 -0400

    feat: enable volumes on the docker driver

--- services/nomad/default.nix
+++ services/nomad/default.nix
@@ -80,6 +80,9 @@
         docker = {
           config = {
             allow_privileged = true;
+            volumes = {
+              enabled = true;
+            };
           };
         };
       };
